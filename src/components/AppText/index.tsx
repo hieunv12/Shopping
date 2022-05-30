6c6ad0c55220cf6eb092efbc6a39916964bb0038
ad-m5 <adamo@Adamos-MacBook-Pro.local>
2022-05-30 15:23:19 +0700

    add AppText

---
+++ src/components/AppText/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextTicker from 'react-native-text-ticker';
+
+<TextTicker
+  duration={6000}
+  loop
+  bounce
+  repeatSpacer={100}
+  marqueeDelay={700}
+></TextTicker>;
