36976cc024fed699658c1bc1076710907882d0a5
Andrew Tran <cao.tran@adamosoft.com>
2022-09-05 11:38:15 +0700

    add Header

--- src/assets/icon/index.ts
+++ src/assets/icon/index.ts
@@ -5,6 +5,8 @@ export * from './searchIcon';
 export * from './downIcon3D';
 export * from './flash3D';
 
+import BackIcon from './svg/backLeft.svg';
+
 import DiscoverIcon from './tab/discover.svg';
 import MainIcon from './tab/home.svg';
 import MarketIcon from './tab/market.svg';
@@ -14,5 +16,5 @@ import IconLiked from './iconLiked.svg';
 import Eye from './eye.svg';
 import EyeActive from './eyeActive.svg';
 
-export {DiscoverIcon, MainIcon, MarketIcon, TradeIcon, UserIcon, IconLiked, Eye, EyeActive};
+export {DiscoverIcon, MainIcon, MarketIcon, TradeIcon, UserIcon, IconLiked, Eye, EyeActive, BackIcon};
 export * from '../icon/image';
