ccaac365ca9f817d00735c1b75f2f3404e5f1918
Hieunv14 <hieunvmob@gmail.com>
2022-11-16 23:21:32 +0700

    up

--- src/components/AppSlide/AppSlide.tsx
+++ src/components/AppSlide/AppSlide.tsx
@@ -1,4 +1,4 @@
-import React, {Component, useRef} from "react";
+import React, {Component, useRef, useState} from "react";
 import {
     SafeAreaView,
     ScrollView,
@@ -7,9 +7,9 @@ import {
     View,
     ImageBackground,
     Animated,
-    Dimensions, useWindowDimensions, Image
+    Dimensions, useWindowDimensions, Image, FlatList, TouchableOpacity
 } from "react-native";
-import {Spacing} from "@theme";
+import {Colors, Spacing} from "@theme";
 import {Device} from "../../assets/device";
 
 
@@ -19,40 +19,72 @@ type AppSlideType={
 }
 export const AppSlide = (props:AppSlideType) => {
     const {images}=props
-    const scrollX = useRef();
-
+    const scrollX = useRef(new Animated.Value(0)).current;
+    const [numImage,setNumImage]=useState(0)
+    const refFlatList=useRef<any>()
     const {width: windowWidth} = useWindowDimensions();
-    console.log(scrollX)
+    const onViewCallBack = React.useCallback((viewableItems)=> {
+        const num:number=viewableItems?.viewableItems[0]?.index
+        setNumImage(num)
+    }, []) // any dependencies that require the function to be "redeclared"
+    const viewConfigRef = React.useRef({ viewAreaCoveragePercentThreshold: 50 })
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.scrollContainer}>
-                <ScrollView
+                <FlatList
+                    data={images?images:[]}
+                    showsHorizontalScrollIndicator={false}
                     horizontal={true}
                     pagingEnabled
-                    showsHorizontalScrollIndicator={false}
-                //     onScroll={(event)=>{
-                //         console.log('data:',event)
-                //     }
-                // }
-                    scrollEventThrottle={1}
-                >
-                    {images.map((image:string, imageIndex:number) => {
-                        return (
-                            <View
-                                key={`banner_product_${imageIndex}`}
-                                style={{width: windowWidth, height:  Device.height/2}}
+                    ref={refFlatList}
+                    initialScrollIndex={numImage}
+                    onScrollToIndexFailed={info => {
+                        const wait = new Promise(resolve => setTimeout(resolve, 0));
+                        wait.then(() => {
+                            refFlatList.current?.scrollToIndex({
+                                index: info.index,
+                                animated: true,
+                            });
+                        });
+                    }}
+                    renderItem={({item,index})=>{
+                    return(
+                        <View
+                            style={{width: windowWidth, height:  Device.height/2}}
 
-                            >
-                                <ImageBackground resizeMode={'contain'} source={{uri: image}} style={styles.card} >
-                                </ImageBackground>
-                            </View>
-                        );
-                    })}
-                </ScrollView>
+                        >
+                            <Image resizeMode={'contain'} source={{uri: item}} style={styles.card}/>
+
+                        </View>
+                    )
+                }}
+                    onViewableItemsChanged={onViewCallBack}
+                    viewabilityConfig={viewConfigRef.current}
+                    scrollEventThrottle={1}
+                />
                 <View style={styles.indicatorContainer}>
-                    {images.map((image:string, imageIndex:number) => {
+                    {images?.map((image:string, imageIndex:number) => {
+                        const width = scrollX.interpolate({
+                            inputRange: [0,1],
+                            outputRange: [1,0],
+                            extrapolate: "clamp"
+                        });
+                        console.log("width:",width)
                         return (
-                            <Image  key={imageIndex} source={{uri:image}} style={{width:Spacing.width50,height:Spacing.height50,marginLeft:Spacing.width8}}/>
+                            <TouchableOpacity onPress={()=>{
+                                setTimeout(() => {
+                                    setNumImage(imageIndex)
+                                    refFlatList?.current?.scrollToIndex({
+                                        animated: true,
+                                        index: imageIndex,
+                                    });
+                                }, 0);
+
+                            }}>
+                                <Animated.Image  key={imageIndex}
+                                                 source={{uri:image}}
+                                                 style={{width:Spacing.width50,height:Spacing.height50,marginLeft:Spacing.width8,borderWidth:imageIndex===numImage?2:0,borderColor:Colors.colorMain}}/>
+                            </TouchableOpacity>
                         );
                     })}
                 </View>
